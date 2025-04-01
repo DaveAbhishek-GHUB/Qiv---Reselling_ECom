21726ed172167062e547fd082530cfa3c2d59769
Rafik-Lakhani <lakhanirafik111@gmail.com>
2025-04-01 12:28:13 +0530

    feat: client and server setup

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port,()=>console.log(`Server is running on port ${port}`));
